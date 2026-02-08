4a7a0942727302ed9fb76c700cb535360e8760ff
NaviCo-de <aridarrell30@gmail.com>
2026-02-09 00:05:12 +0700

    Edit dependencies in build.gradle.kts

--- eshop/build.gradle.kts
+++ eshop/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
 tasks.register<Test>("unitTest") {
