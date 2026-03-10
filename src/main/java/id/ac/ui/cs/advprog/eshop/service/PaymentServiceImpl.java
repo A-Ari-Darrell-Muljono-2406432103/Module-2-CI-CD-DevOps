1d5706aa788f84587b903a6654e402e6818d379e
NaviCo-de <aridarrell30@gmail.com>
2026-03-10 19:40:53 +0700

    [GREEN] Implement Payment service

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class PaymentServiceImpl implements PaymentService {
+    private final PaymentRepository paymentRepository;
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        Payment payment = new Payment(UUID.randomUUID().toString(), order, method, paymentData);
+        return paymentRepository.save(payment);
+    }
+}
