e176712248edf79b5c200eac36119db77a0350fb
NaviCo-de <aridarrell30@gmail.com>
2026-03-10 19:39:33 +0700

    [GREEN] Implement Payment repository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,30 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentRepository {
+    private List<Payment> payments = new ArrayList<>();
+
+    public Payment save(Payment payment) {
+        for (int i = 0; i < payments.size(); i++) {
+            if (payments.get(i).getId().equals(payment.getId())) {
+                payments.set(i, payment);
+                return payment;
+            }
+        }
+        payments.add(payment);
+        return payment;
+    }
+
+    public Payment findById(String id) {
+        for (Payment payment : payments) {
+            if (payment.getId().equals(id)) {
+                return payment;
+            }
+        }
+        return null;
+    }
+}
